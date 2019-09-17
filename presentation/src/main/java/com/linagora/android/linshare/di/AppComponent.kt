3dd8c779ebc094316b583913f1133993315db5c3
Dat PHAM HOANG <dphamhoang@linagora.com>
2019-09-17 16:18:42 +0700

    LA-22 Add Dagger with AppComponent and AppModule

---
+++ presentation/src/main/java/com/linagora/android/linshare/di/AppComponent.kt
@@ -0,0 +1,22 @@
+package com.linagora.android.linshare.di
+
+import com.linagora.android.linshare.view.LinShareApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        AndroidSupportInjectionModule::class,
+        AppModule::class
+    ]
+)
+interface AppComponent : AndroidInjector<LinShareApplication> {
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance application: LinShareApplication): AppComponent
+    }
+}
