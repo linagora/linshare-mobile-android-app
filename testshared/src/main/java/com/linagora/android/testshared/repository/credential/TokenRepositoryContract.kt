cad78131387893b31575f831467a0a8bc6885d9f
hoangdat <hoangdat.pham2911@gmail.com>
2019-10-07 11:52:20 +0700

    LA-19 Add TestFixtures to share some constants in test

--- testshared/src/main/java/com/linagora/android/testshared/repository/credential/TokenRepositoryContract.kt
+++ testshared/src/main/java/com/linagora/android/testshared/repository/credential/TokenRepositoryContract.kt
@@ -1,17 +1,13 @@
 package com.linagora.android.testshared.repository.credential
 
 import com.google.common.truth.Truth.assertThat
-import com.linagora.android.linshare.domain.model.Token
 import com.linagora.android.linshare.domain.repository.TokenRepository
+import com.linagora.android.testshared.TestFixtures.Tokens.TOKEN
+import com.linagora.android.testshared.TestFixtures.Tokens.TOKEN_2
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.jupiter.api.Test
 
 abstract class TokenRepositoryContract {
-
-    val TOKEN_VALUE = "ZHBoYW1ob2FuZ0BsaW5hZ29yYS5jb206aThqckJ3KTgzNk4="
-    val TOKEN_VALUE_2 = "ZHBoYW1ob2GzX0BsaW5hZ29yYS5jb206aThqckJ3KTgzNk4="
-    private val TOKEN = Token(TOKEN_VALUE)
-    private val TOKEN_2 = Token(TOKEN_VALUE_2)
 
     abstract val tokenRepository: TokenRepository
 
