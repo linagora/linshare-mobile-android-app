93d9a68c0a967fdd9ac2d84f750842a22805e005
Dat PHAM HOANG <dphamhoang@linagora.com>
2019-10-16 17:25:25 +0700

    LA-20 Add ServicePath and Endpoint for linshare service route path

---
+++ presentation/src/test/java/com/linagora/android/linshare/network/URLExtensionTest.kt
@@ -0,0 +1,30 @@
+package com.linagora.android.linshare.network
+
+import com.google.common.truth.Truth.assertThat
+import com.linagora.android.linshare.network.Endpoint.AUTHENTICATION_PATH
+import com.linagora.android.linshare.util.withServicePath
+import org.junit.jupiter.api.Test
+import java.net.URL
+
+class URLExtensionTest {
+
+    private val BASE_PATH = "http://localhost.com"
+    private val BASE_URL = URL(BASE_PATH)
+
+    private val BASE_PATH_2 = "http://localhost.com/"
+    private val BASE_URL_2 = URL(BASE_PATH_2)
+
+    @Test
+    fun withServicePathShouldReturnASuccessEndpoint() {
+        val endpoint = BASE_URL.withServicePath(Endpoint.AUTHENTICAION)
+
+        assertThat(endpoint).isEqualTo(URL(BASE_PATH.plus("/$AUTHENTICATION_PATH")))
+    }
+
+    @Test
+    fun withServicePathShouldReturnASuccessEndpointWithSplashEnd() {
+        val endpoint = BASE_URL_2.withServicePath(Endpoint.AUTHENTICAION)
+
+        assertThat(endpoint).isEqualTo(URL(BASE_PATH_2.plus("$AUTHENTICATION_PATH")))
+    }
+}
