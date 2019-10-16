8caee0cdc119c760d53f53b581fa01c253770826
hoangdat <hoangdat.pham2911@gmail.com>
2019-10-16 17:22:06 +0700

    LA-20 Add BaseViewModel with consume/dispatch state to View

---
+++ presentation/src/main/java/com/linagora/android/linshare/util/CoroutinesDispatcherProvider.kt
@@ -0,0 +1,15 @@
+package com.linagora.android.linshare.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import javax.inject.Inject
+
+data class CoroutinesDispatcherProvider(
+    val main: CoroutineDispatcher,
+    val computation: CoroutineDispatcher,
+    val io: CoroutineDispatcher
+) {
+    @Inject constructor() : this(Main, Default, IO)
+}
