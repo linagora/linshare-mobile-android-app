e17257056950fc4e16fb46aaa91960cd1715bd3d
Dat PHAM HOANG <dphamhoang@linagora.com>
2019-10-22 17:10:53 +0700

    LA-23 Add NavigationHost to setup toolbar with navigation

--- presentation/src/main/java/com/linagora/android/linshare/view/MainActivity.kt
+++ presentation/src/main/java/com/linagora/android/linshare/view/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.linagora.android.linshare.view
 
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.linagora.android.linshare.R
 import com.linagora.android.linshare.util.getViewModel
 import com.linagora.android.linshare.view.base.BaseActivity
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), NavigationHost {
 
+    companion object {
+        private val EMPTY_DRAWER: DrawerLayout? = null
+
+        private val TOP_LEVEL_DESTINATIONS = setOf(
+            R.id.wizardFragment
+        )
+    }
+
+    private lateinit var navController: NavController
     private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +30,16 @@ class MainActivity : BaseActivity() {
         viewModel = getViewModel(viewModelFactory)
 
         setContentView(R.layout.activity_main)
+
+        retrieveNavigationController()
+    }
+
+    private fun retrieveNavigationController() {
+        navController = findNavController(R.id.nav_host_fragment)
+    }
+
+    override fun registerToolbarWithNavigation(toolbar: Toolbar) {
+        val appBarConfiguration = AppBarConfiguration(TOP_LEVEL_DESTINATIONS, EMPTY_DRAWER)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
--- presentation/src/main/java/com/linagora/android/linshare/view/MainNavigationFragment.kt
+++ presentation/src/main/java/com/linagora/android/linshare/view/MainNavigationFragment.kt
@@ -1,5 +1,41 @@
 package com.linagora.android.linshare.view
 
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import com.linagora.android.linshare.R
 import dagger.android.support.DaggerFragment
 
-open class MainNavigationFragment : DaggerFragment()
+/**
+ * To be implemented by components that host top-level navigation destinations.
+ */
+interface NavigationHost {
+
+    /** Called by MainNavigationFragment to setup it's toolbar with the navigation controller. */
+    fun registerToolbarWithNavigation(toolbar: Toolbar)
+}
+
+open class MainNavigationFragment : DaggerFragment() {
+    protected var navigationHost: NavigationHost? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is NavigationHost) {
+            navigationHost = context
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        navigationHost = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val host = navigationHost ?: return
+        val mainToolbar: Toolbar = view.findViewById(R.id.toolbar) ?: return
+        mainToolbar.apply {
+            host.registerToolbarWithNavigation(this)
+        }
+    }
+}
