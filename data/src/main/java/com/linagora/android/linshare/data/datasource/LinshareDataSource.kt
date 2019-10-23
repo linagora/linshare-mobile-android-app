4918943fd4602fb84212e8e43e234fbd25f8750e
Dat PHAM HOANG <dphamhoang@linagora.com>
2019-10-23 15:44:44 +0700

    LA-23 Add Timeout error logic with Authenticate use-case

--- data/src/main/java/com/linagora/android/linshare/data/datasource/LinshareDataSource.kt
+++ data/src/main/java/com/linagora/android/linshare/data/datasource/LinshareDataSource.kt
@@ -4,10 +4,20 @@ import com.linagora.android.linshare.data.api.LinshareApi
 import com.linagora.android.linshare.domain.model.Password
 import com.linagora.android.linshare.domain.model.Token
 import com.linagora.android.linshare.domain.model.Username
+import com.linagora.android.linshare.domain.usecases.auth.AuthenticationException
+import com.linagora.android.linshare.domain.usecases.auth.AuthenticationException.Companion.WRONG_CREDENTIAL
 import com.linagora.android.linshare.domain.usecases.auth.BadCredentials
+import com.linagora.android.linshare.domain.usecases.auth.ConnectError
 import com.linagora.android.linshare.domain.usecases.auth.EmptyToken
+import com.linagora.android.linshare.domain.usecases.auth.ServerNotFound
+import com.linagora.android.linshare.domain.usecases.auth.UnknownError
+import kotlinx.coroutines.TimeoutCancellationException
 import okhttp3.Credentials
+import retrofit2.Response
+import java.net.SocketException
+import java.net.SocketTimeoutException
 import java.net.URL
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 class LinshareDataSource @Inject constructor(
@@ -15,12 +25,33 @@ class LinshareDataSource @Inject constructor(
 ) {
 
     suspend fun retrievePermanentToken(baseUrl: URL, username: Username, password: Password): Token {
-        val response = linshareApi.getPermanentToken(
-            authenticateUrl = baseUrl.toString(),
-            basicToken = Credentials.basic(username.username, password.value))
-        return when (response.isSuccessful) {
-            true -> response.body() ?: throw EmptyToken
-            else -> throw BadCredentials(response.message())
+        try {
+            val response = linshareApi.getPermanentToken(
+                authenticateUrl = baseUrl.toString(),
+                basicToken = Credentials.basic(username.username, password.value)
+            )
+            return when (response.isSuccessful) {
+                true -> response.body() ?: throw EmptyToken
+                else -> throw produceError(response)
+            }
+        } catch (exp: Exception) {
+            when (exp) {
+                is EmptyToken, ServerNotFound -> throw exp
+                is BadCredentials -> throw exp
+                is SocketTimeoutException -> throw ConnectError
+                is SocketException -> throw ConnectError
+                is TimeoutCancellationException -> throw ConnectError
+                is UnknownHostException -> throw ConnectError
+                else -> throw UnknownError
+            }
+        }
+    }
+
+    private fun produceError(response: Response<Token>): AuthenticationException {
+        return when (response.code()) {
+            404 -> ServerNotFound
+            401 -> BadCredentials(WRONG_CREDENTIAL)
+            else -> UnknownError
         }
     }
 }
--- domain/src/main/java/com/linagora/android/linshare/domain/usecases/auth/AuthenticationException.kt
+++ domain/src/main/java/com/linagora/android/linshare/domain/usecases/auth/AuthenticationException.kt
@@ -8,8 +8,17 @@ sealed class AuthenticationException(message: String) : RuntimeException(message
         const val WRONG_CREDENTIAL = "Credential is wrong"
 
         const val WRONG_PASSWORD = "Password is wrong"
+
+        const val SERVER_NOT_FOUND = "Server not found"
+
+        const val CONNECT_ERROR = "Connect error"
+
+        const val UNKNOWN = "Unknown error"
     }
 }
 
 data class BadCredentials(override val message: String) : AuthenticationException(message)
 object EmptyToken : AuthenticationException(EMPTY_TOKEN)
+object ServerNotFound : AuthenticationException(SERVER_NOT_FOUND)
+object ConnectError : AuthenticationException(CONNECT_ERROR)
+object UnknownError : AuthenticationException(UNKNOWN)
