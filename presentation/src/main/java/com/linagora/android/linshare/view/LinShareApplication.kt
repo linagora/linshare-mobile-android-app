4253b138a05b584390d245f5d1d48a725e3b313a
hoangdat <hoangdat.pham2911@gmail.com>
2019-10-29 06:36:02 +0700

    LA-23 Add Timber to plant tree log

--- presentation/src/main/java/com/linagora/android/linshare/view/LinShareApplication.kt
+++ presentation/src/main/java/com/linagora/android/linshare/view/LinShareApplication.kt
@@ -1,10 +1,20 @@
 package com.linagora.android.linshare.view
 
+import com.linagora.android.linshare.BuildConfig
 import com.linagora.android.linshare.inject.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import timber.log.Timber
 
 class LinShareApplication : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.factory().create(this)
