0f5a92e228399f73dc1cf59574e3627126e3b773
Dat PHAM HOANG <dphamhoang@linagora.com>
2019-11-01 17:41:53 +0700

    LA-36 Add AccountDetailViewModel

--- presentation/src/main/java/com/linagora/android/linshare/view/accounts/AccountDetailModule.kt
+++ presentation/src/main/java/com/linagora/android/linshare/view/accounts/AccountDetailModule.kt
@@ -1,8 +1,12 @@
 package com.linagora.android.linshare.view.accounts
 
+import androidx.lifecycle.ViewModel
 import com.linagora.android.linshare.inject.annotation.FragmentScoped
+import com.linagora.android.linshare.inject.annotation.ViewModelKey
+import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoMap
 
 @Module
 internal abstract class AccountDetailModule {
@@ -10,4 +14,9 @@ internal abstract class AccountDetailModule {
     @FragmentScoped
     @ContributesAndroidInjector
     internal abstract fun contributeAccountDetailFragment(): AccountDetailsFragment
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(AccountDetailsViewModel::class)
+    abstract fun bindAccountDetailViewModel(accountDetailsViewModel: AccountDetailsViewModel): ViewModel
 }
