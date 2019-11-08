9c935dd14b8d483569820d7668f7a72f297efac7
Dat PHAM HOANG <dphamhoang@linagora.com>
2019-11-08 13:54:47 +0700

    LA-34 Add get authorized user and quota in GetAccountDetailsInteractor

--- domain/src/main/java/com/linagora/android/linshare/domain/usecases/account/AccountDetailsViewState.kt
+++ domain/src/main/java/com/linagora/android/linshare/domain/usecases/account/AccountDetailsViewState.kt
@@ -1,15 +1,19 @@
 package com.linagora.android.linshare.domain.usecases.account
 
+import com.linagora.android.linshare.domain.model.AccountQuota
 import com.linagora.android.linshare.domain.model.Credential
 import com.linagora.android.linshare.domain.model.LastLogin
 import com.linagora.android.linshare.domain.model.Token
+import com.linagora.android.linshare.domain.model.User
 import com.linagora.android.linshare.domain.usecases.utils.Success
 
 data class AccountDetailsViewState(
     val isLoading: Boolean = false,
     val credential: Credential? = null,
     val token: Token? = null,
-    val lastLogin: LastLogin? = null
+    val lastLogin: LastLogin? = null,
+    val user: User? = null,
+    val quota: AccountQuota? = null
 ) : Success.ViewState() {
 
     companion object {
