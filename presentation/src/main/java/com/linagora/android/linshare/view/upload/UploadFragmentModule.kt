6eda00f327b1b6ac85b6f8b4aab33f6ffcbf7c00
hoangdat <hoangdat.pham2911@gmail.com>
2019-12-04 16:09:29 +0700

    LA-42 Binding UploadWorker

--- presentation/src/main/java/com/linagora/android/linshare/view/upload/UploadFragmentModule.kt
+++ presentation/src/main/java/com/linagora/android/linshare/view/upload/UploadFragmentModule.kt
@@ -1,12 +1,8 @@
 package com.linagora.android.linshare.view.upload
 
-import androidx.lifecycle.ViewModel
 import com.linagora.android.linshare.inject.annotation.FragmentScoped
-import com.linagora.android.linshare.inject.annotation.ViewModelKey
-import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import dagger.multibindings.IntoMap
 
 @Module
 internal abstract class UploadFragmentModule {
@@ -14,9 +10,4 @@ internal abstract class UploadFragmentModule {
     @FragmentScoped
     @ContributesAndroidInjector
     internal abstract fun contributeUploadFragment(): UploadFragment
-
-    @Binds
-    @IntoMap
-    @ViewModelKey(UploadFragmentViewModel::class)
-    abstract fun bindUploadFragmentViewModel(uploadFragmentViewModel: UploadFragmentViewModel): ViewModel
 }
