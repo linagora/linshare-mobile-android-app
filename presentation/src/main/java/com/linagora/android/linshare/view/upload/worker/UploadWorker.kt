42cdc58675b099f2ae807f1cdbc48605eaeb4742
hoangdat <hoangdat.pham2911@gmail.com>
2019-12-04 16:11:23 +0700

    LA-42 Simple Notification to indicate Upload in background task

--- presentation/src/main/java/com/linagora/android/linshare/view/upload/worker/UploadWorker.kt
+++ presentation/src/main/java/com/linagora/android/linshare/view/upload/worker/UploadWorker.kt
@@ -11,7 +11,7 @@ import com.linagora.android.linshare.R
 import com.linagora.android.linshare.domain.model.document.DocumentRequest
 import com.linagora.android.linshare.domain.repository.document.DocumentRepository
 import com.linagora.android.linshare.inject.worker.ChildWorkerFactory
-import com.linagora.android.linshare.util.makeStatusNotification
+import com.linagora.android.linshare.util.pushSystemNotification
 import okhttp3.MediaType.Companion.toMediaType
 import org.slf4j.LoggerFactory
 import javax.inject.Inject
@@ -33,25 +33,28 @@ class UploadWorker(
             val fileUri = Uri.parse(inputData.getString(FILE_URI_INPUT_KEY))
             queryDocumentFromSystemFile(fileUri)!!
                 .let { document ->
-                    makeStatusNotification(
+                    pushSystemNotification(
+                        title = appContext.getString(R.string.uploading_title),
                         message = String.format(appContext.getString(R.string.uploading_file), document.fileName),
                         context = appContext
                     )
                     documentRepository.upload(document)
-                    makeStatusNotification(
+                    pushSystemNotification(
+                        title = appContext.getString(R.string.upload_finished_title),
                         message = String.format(appContext.getString(R.string.upload_success), document.fileName),
                         context = appContext
                     )
                 }
+            return Result.success()
         } catch (exp: Exception) {
             LOGGER.error(exp.message, exp)
-            makeStatusNotification(
+            pushSystemNotification(
+                title = appContext.getString(R.string.upload_finished_title),
                 message = String.format(appContext.getString(R.string.upload_failed)),
                 context = appContext
             )
             return Result.failure()
         }
-        return Result.success()
     }
 
     private fun queryDocumentFromSystemFile(uri: Uri): DocumentRequest? {
