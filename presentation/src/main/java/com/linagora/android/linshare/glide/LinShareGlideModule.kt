ae01f48e12fb9497b7e15af2af9a06b5e2b65c43
Dat PHAM HOANG <dphamhoang@linagora.com>
2019-12-16 10:57:41 +0700

    LA-44 Add Glide library and module for app

---
+++ presentation/src/main/java/com/linagora/android/linshare/glide/LinShareGlideModule.kt
@@ -0,0 +1,7 @@
+package com.linagora.android.linshare.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class LinShareGlideModule : AppGlideModule()
