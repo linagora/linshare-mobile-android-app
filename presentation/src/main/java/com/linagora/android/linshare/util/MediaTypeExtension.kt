09c5b2ba842a73ce21ba49be6719b9d5b2f97981
hoangdat <hoangdat.pham2911@gmail.com>
2019-12-31 14:47:58 +0700

    LA-66 Binding Document icon based on MediaType

--- presentation/src/main/java/com/linagora/android/linshare/util/MediaTypeExtension.kt
+++ presentation/src/main/java/com/linagora/android/linshare/util/MediaTypeExtension.kt
@@ -9,9 +9,17 @@ object MimeType {
 
 fun MediaType.getDrawableIcon(): Int {
     return when (type) {
-        "video" -> R.drawable.ic_movie_48px
-        "image" -> R.drawable.ic_photo_48px
+        "video" -> R.drawable.ic_movie
+        "image" -> R.drawable.ic_picture
         "audio" -> R.drawable.ic_audiotrack_48px
+        "application" -> getApplicationIconBaseOnSubType()
+        else -> R.drawable.ic_file
+    }
+}
+
+fun MediaType.getApplicationIconBaseOnSubType(): Int {
+    return when (subtype) {
+        "pdf" -> R.drawable.ic_pdf
         else -> R.drawable.ic_file
     }
 }
