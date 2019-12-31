f000945f81abe583b46682d9fcce55327dff6235
Dat PHAM HOANG <dphamhoang@linagora.com>
2019-12-31 11:32:08 +0700

    LA-65 Update TimeUtils convert method with format parameter

--- presentation/src/test/java/com/linagora/android/linshare/utils/TimeUtilsTest.kt
+++ presentation/src/test/java/com/linagora/android/linshare/utils/TimeUtilsTest.kt
@@ -2,6 +2,8 @@ package com.linagora.android.linshare.utils
 
 import com.google.common.truth.Truth.assertThat
 import com.linagora.android.linshare.util.TimeUtils
+import com.linagora.android.linshare.util.TimeUtils.LinShareTimeFormat.LastLoginFormat
+import com.linagora.android.linshare.util.TimeUtils.LinShareTimeFormat.LastModifiedFormat
 import com.linagora.android.testshared.TestFixtures.Accounts.LAST_LOGIN_DATE
 import org.junit.jupiter.api.Test
 import java.time.format.DateTimeFormatter
@@ -9,10 +11,17 @@ import java.time.format.DateTimeFormatter
 class TimeUtilsTest {
 
     @Test
-    fun convertToLocalTimeShouldFollowTheFormat() {
-        val dateTime = TimeUtils.convertToLocalTime(LAST_LOGIN_DATE)
+    fun convertToLocalTimeShouldFollowLastLoginFormat() {
+        val dateTime = TimeUtils.convertToLocalTime(LAST_LOGIN_DATE, LastLoginFormat)
 
         assertThat(isValidFormat("dd.MM.YYYY hh:mm a", dateTime)).isTrue()
+    }
+
+    @Test
+    fun convertToLocalTimeShouldFollowLastModifiedFormat() {
+        val dateTime = TimeUtils.convertToLocalTime(LAST_LOGIN_DATE, LastModifiedFormat)
+
+        assertThat(isValidFormat("dd MMM YYYY HH:mm", dateTime)).isTrue()
     }
 
     private fun isValidFormat(format: String, value: String): Boolean {
