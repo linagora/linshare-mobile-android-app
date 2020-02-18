22ef4927fb2cdcd774213b2da3bb8a7724fc7a49
Truong Ngoc Duc <ndtruong@linagora.com>
2020-02-18 16:11:11 +0700

    LA-104 Refactor code and fix get document request failed before upload

---
+++ presentation/src/main/java/com/linagora/android/linshare/util/CursorExtensions.kt
@@ -0,0 +1,19 @@
+package com.linagora.android.linshare.util
+
+import android.database.Cursor
+import android.net.Uri
+import android.provider.MediaStore
+import android.provider.OpenableColumns
+import com.linagora.android.linshare.domain.model.document.DocumentRequest
+
+fun Cursor.getDocumentRequest(uri: Uri): DocumentRequest? {
+    moveToFirst()
+    val fileName = getString(getColumnIndex(OpenableColumns.DISPLAY_NAME))
+    val size = getLong(getColumnIndex(OpenableColumns.SIZE))
+    val mediaType = getColumnIndex(MediaStore.Images.Media.MIME_TYPE)
+        .takeIf { columnIndex -> columnIndex > -1 }
+        ?.let { columnIndex -> getString(columnIndex) }
+        ?.let { mimeType -> fileName.getMediaType(mimeType) }
+        ?: fileName.getMediaTypeFromExtension()
+    return DocumentRequest(uri, fileName, size, mediaType)
+}
--- presentation/src/main/java/com/linagora/android/linshare/util/MediaTypeExtension.kt
+++ presentation/src/main/java/com/linagora/android/linshare/util/MediaTypeExtension.kt
@@ -1,7 +1,9 @@
 package com.linagora.android.linshare.util
 
+import android.webkit.MimeTypeMap
 import com.linagora.android.linshare.R
 import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 
 object MimeType {
     const val APPLICATION_DEFAULT = "application/octet-stream"
@@ -22,4 +24,16 @@ fun MediaType.getApplicationIconBaseOnSubType(): Int {
         "pdf" -> R.drawable.ic_pdf
         else -> R.drawable.ic_file
     }
+}
+
+fun String.getMediaType(defaultMimeType: String): MediaType {
+    return runCatching { defaultMimeType.toMediaType() }
+        .getOrElse { this.getMediaTypeFromExtension() }
+}
+
+fun String.getMediaTypeFromExtension(): MediaType {
+    return MimeTypeMap.getFileExtensionFromUrl(this)
+        ?.let { extensions -> MimeTypeMap.getSingleton().getMimeTypeFromExtension(extensions) }
+        ?.let { mediaType -> mediaType.toMediaType() }
+        ?: MimeType.APPLICATION_DEFAULT.toMediaType()
 }
