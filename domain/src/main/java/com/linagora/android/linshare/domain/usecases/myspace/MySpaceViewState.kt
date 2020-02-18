f090d34bdf7078f41367dbd72cb11accbca1ef4a
hoangdat <hoangdat.pham2911@gmail.com>
2020-02-18 14:45:46 +0700

    LA-109 Implement handling click on each item in MySpace list

--- domain/src/main/java/com/linagora/android/linshare/domain/usecases/myspace/MySpaceViewState.kt
+++ domain/src/main/java/com/linagora/android/linshare/domain/usecases/myspace/MySpaceViewState.kt
@@ -6,3 +6,4 @@ import com.linagora.android.linshare.domain.usecases.utils.Success
 
 data class MySpaceViewState(val documents: List<Document>) : Success.ViewState()
 data class MySpaceFailure(val throwable: Throwable) : FeatureFailure()
+data class ContextMenuClick(val document: Document) : Success.ViewEvent()
--- presentation/src/main/java/com/linagora/android/linshare/view/myspace/MySpaceViewModel.kt
+++ presentation/src/main/java/com/linagora/android/linshare/view/myspace/MySpaceViewModel.kt
@@ -1,16 +1,27 @@
 package com.linagora.android.linshare.view.myspace
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import arrow.core.Either
+import com.linagora.android.linshare.domain.model.document.Document
+import com.linagora.android.linshare.domain.usecases.myspace.ContextMenuClick
 import com.linagora.android.linshare.domain.usecases.myspace.GetAllDocumentsInteractor
 import com.linagora.android.linshare.util.CoroutinesDispatcherProvider
 import com.linagora.android.linshare.view.base.BaseViewModel
 import kotlinx.coroutines.launch
+import org.slf4j.LoggerFactory
 import javax.inject.Inject
 
 class MySpaceViewModel @Inject constructor(
     private val getAllDocumentsInteractor: GetAllDocumentsInteractor,
     private val dispatcherProvider: CoroutinesDispatcherProvider
 ) : BaseViewModel(dispatcherProvider) {
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(MySpaceViewModel::class.java)
+    }
+
+    private val downloadingDocument = MutableLiveData<Document>()
 
     fun getAllDocuments() {
         viewModelScope.launch(dispatcherProvider.io) {
@@ -20,5 +31,19 @@ class MySpaceViewModel @Inject constructor(
 
     fun onSwipeRefresh() {
         getAllDocuments()
+    }
+
+    fun onContextMenuClick(document: Document) {
+        LOGGER.info("onContextMenuClick() $document")
+        setProcessingDocument(document)
+        dispatchState(Either.right(ContextMenuClick(document)))
+    }
+
+    private fun setProcessingDocument(document: Document) {
+        downloadingDocument.value = document
+    }
+
+    fun getDownloadingDocument(): Document? {
+        return downloadingDocument.value
     }
 }
