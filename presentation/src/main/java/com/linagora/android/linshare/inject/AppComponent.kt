dd1cf625ae6aac5575a71fe0bd627100b5015ba9
Dat PHAM HOANG <dphamhoang@linagora.com>
2020-02-24 17:03:58 +0700

    LA-106 Implement DownloadingRepository by Room

--- presentation/src/main/java/com/linagora/android/linshare/inject/AppComponent.kt
+++ presentation/src/main/java/com/linagora/android/linshare/inject/AppComponent.kt
@@ -14,6 +14,7 @@ import javax.inject.Singleton
     modules = [
         AndroidSupportInjectionModule::class,
         AppModule::class,
+        DatabaseModule::class,
         DocumentModule::class,
         ActivityBindingModule::class,
         ViewModelModule::class,
