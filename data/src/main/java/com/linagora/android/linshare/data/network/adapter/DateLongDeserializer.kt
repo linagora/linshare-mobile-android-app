a7d2cf776cc9c63464caab9a6bd6e04aa7adb7a7
Truong Ngoc Duc <ndtruong@linagora.com>
2020-03-10 11:31:48 +0700

    LA-113 Add serialize for Date and Media Type of Document

--- data/src/main/java/com/linagora/android/linshare/data/network/adapter/DateLongDeserializer.kt
+++ data/src/main/java/com/linagora/android/linshare/data/network/adapter/DateLongDeserializer.kt
@@ -3,10 +3,21 @@ package com.linagora.android.linshare.data.network.adapter
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
 import java.lang.reflect.Type
 import java.util.Date
 
-class DateLongDeserializer : JsonDeserializer<Date> {
+class DateLongDeserializer : JsonDeserializer<Date>, JsonSerializer<Date> {
+    override fun serialize(
+        src: Date?,
+        typeOfSrc: Type?,
+        context: JsonSerializationContext?
+    ): JsonElement {
+        return JsonPrimitive(src!!.time)
+    }
+
     override fun deserialize(
         json: JsonElement?,
         typeOfT: Type?,
