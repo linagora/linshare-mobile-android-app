355797cc791dcea4efa476ab149c18a7edd9d5a2
hoangdat <hoangdat.pham2911@gmail.com>
2020-04-08 14:05:19 +0700

    LA-130 Download For ReceivedShare item

--- presentation/src/main/java/com/linagora/android/linshare/view/receivedshares/ReceivedSharesFragment.kt
+++ presentation/src/main/java/com/linagora/android/linshare/view/receivedshares/ReceivedSharesFragment.kt
@@ -1,5 +1,7 @@
 package com.linagora.android.linshare.view.receivedshares
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,14 +12,17 @@ import androidx.lifecycle.ViewModelProvider
 import arrow.core.Either
 import com.linagora.android.linshare.R
 import com.linagora.android.linshare.databinding.FragmentReceivedSharesBinding
+import com.linagora.android.linshare.domain.model.properties.PreviousUserPermissionAction.DENIED
 import com.linagora.android.linshare.domain.model.share.Share
 import com.linagora.android.linshare.domain.usecases.receivedshare.ContextMenuReceivedShareClick
 import com.linagora.android.linshare.domain.usecases.receivedshare.DownloadReceivedShareClick
 import com.linagora.android.linshare.domain.usecases.utils.Success
 import com.linagora.android.linshare.model.permission.PermissionResult
+import com.linagora.android.linshare.model.properties.RuntimePermissionRequest
 import com.linagora.android.linshare.util.getViewModel
 import com.linagora.android.linshare.view.MainActivityViewModel
 import com.linagora.android.linshare.view.MainNavigationFragment
+import com.linagora.android.linshare.view.WriteExternalPermissionRequestCode
 import kotlinx.android.synthetic.main.fragment_received_shares.swipeLayoutReceivedList
 import org.slf4j.LoggerFactory
 import javax.inject.Inject
@@ -54,6 +59,7 @@ class ReceivedSharesFragment : MainNavigationFragment() {
         binding.viewModel = receivedSharesViewModel
 
         observeViewState()
+        observeRequestPermission()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -88,10 +94,25 @@ class ReceivedSharesFragment : MainNavigationFragment() {
 
     private fun download(share: Share) {
         LOGGER.info("download() $share")
+        mainActivityViewModel.currentAuthentication.value
+            ?.let { authentication ->
+                receivedSharesViewModel.downloadShare(authentication.credential, authentication.token, share)
+            }
     }
 
     private fun shouldRequestWriteStoragePermission() {
         mainActivityViewModel.shouldShowWriteStoragePermissionRequest(requireActivity())
+    }
+
+    private fun observeRequestPermission() {
+        mainActivityViewModel.shouldShowPermissionRequestState.observe(viewLifecycleOwner, Observer {
+            if (it is RuntimePermissionRequest.ShouldShowWriteStorage) {
+                requestPermissions(
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    WriteExternalPermissionRequestCode.code
+                )
+            }
+        })
     }
 
     private fun showContextMenuReceivedShare(share: Share) {
@@ -106,5 +127,22 @@ class ReceivedSharesFragment : MainNavigationFragment() {
     private fun getReceivedList() {
         LOGGER.info("getReceivedList")
         receivedSharesViewModel.getReceivedList()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        LOGGER.info("onRequestPermissionsResult() $requestCode")
+        when (requestCode) {
+            WriteExternalPermissionRequestCode.code -> {
+                Either.cond(
+                    test = grantResults.all { grantResult -> grantResult == PackageManager.PERMISSION_GRANTED },
+                    ifTrue = { receivedSharesViewModel.getDownloading()?.let { download(it) } },
+                    ifFalse = { mainActivityViewModel.setActionForWriteStoragePermissionRequest(DENIED) }
+                )
+            }
+        }
     }
 }
