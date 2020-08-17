5d56e1b0448f5f4a0d4d27712e7e66a512d7c844
Ngoc Duc TRUONG <ndtruong@linagora.com>
2020-08-17 16:21:00 +0700

    LA-211 add roles for update adapter

---
+++ presentation/src/main/java/com/linagora/android/linshare/adapter/role/RolesForUpdateAdapter.kt
@@ -0,0 +1,81 @@
+/*
+ * LinShare is an open source filesharing software, part of the LinPKI software
+ * suite, developed by Linagora.
+ *
+ * Copyright (C) 2020 LINAGORA
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version,
+ * provided you comply with the Additional Terms applicable for LinShare software by
+ * Linagora pursuant to Section 7 of the GNU Affero General Public License,
+ * subsections (b), (c), and (e), pursuant to which you must notably (i) retain the
+ * display in the interface of the “LinShare™” trademark/logo, the "Libre & Free" mention,
+ * the words “You are using the Free and Open Source version of LinShare™, powered by
+ * Linagora © 2009–2020. Contribute to Linshare R&D by subscribing to an Enterprise
+ * offer!”. You must also retain the latter notice in all asynchronous messages such as
+ * e-mails sent with the Program, (ii) retain all hypertext links between LinShare and
+ * http://www.linshare.org, between linagora.com and Linagora, and (iii) refrain from
+ * infringing Linagora intellectual property rights over its trademarks and commercial
+ * brands. Other Additional Terms apply, see
+ * <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf>
+ * for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ * You should have received a copy of the GNU Affero General Public License and its
+ * applicable Additional Terms for LinShare along with this program. If not, see
+ * <http://www.gnu.org/licenses/> for the GNU Affero General Public License version
+ *  3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for
+ *  the Additional Terms applicable to LinShare software.
+ */
+
+package com.linagora.android.linshare.adapter.role
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.linagora.android.linshare.adapter.diff.RoleDiffCallback
+import com.linagora.android.linshare.databinding.SelectRoleForUpdateItemBinding
+import com.linagora.android.linshare.domain.model.sharedspace.SharedSpaceRole
+import com.linagora.android.linshare.domain.model.sharedspace.member.SharedSpaceMember
+import com.linagora.android.linshare.view.base.OnSelectRolesForUpdate
+
+class RolesForUpdateAdapter(
+    private val lastSelectedRole: SharedSpaceRole,
+    private val onSelectedRolesForUpdate: OnSelectRolesForUpdate,
+    private val sharedSpaceMember: SharedSpaceMember
+) : ListAdapter<SharedSpaceRole, RoleForUpdateViewHolder>(RoleDiffCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RoleForUpdateViewHolder {
+        return RoleForUpdateViewHolder(
+            SelectRoleForUpdateItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            ),
+            onSelectedRolesForUpdate,
+            sharedSpaceMember
+        )
+    }
+
+    override fun onBindViewHolder(holder: RoleForUpdateViewHolder, position: Int) {
+        return holder.bind(getItem(position), lastSelectedRole)
+    }
+}
+
+class RoleForUpdateViewHolder(
+    private val binding: SelectRoleForUpdateItemBinding,
+    private val onSelectRolesForUpdate: OnSelectRolesForUpdate,
+    private val sharedSpaceMember: SharedSpaceMember
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(sharedSpaceRole: SharedSpaceRole, lastSelectedRole: SharedSpaceRole) {
+        binding.thisRole = sharedSpaceRole
+        binding.lastSelectedRole = lastSelectedRole
+        binding.onSelectRoleForUpdateBehavior = onSelectRolesForUpdate
+        binding.sharedSpaceMember = sharedSpaceMember
+        binding.executePendingBindings()
+    }
+}
